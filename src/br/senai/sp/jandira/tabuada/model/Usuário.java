4cc61c4ff9585b345f7bf041c040bab46b9c266b
qwe <qwe@cfp127aluno.local>
2025-11-03 15:57:33 -0300

    Função de Obter Dados adicionada

--- src/br/senai/sp/jandira/tabuada/model/Usuário.java
+++ src/br/senai/sp/jandira/tabuada/model/Usuário.java
@@ -1,4 +1,26 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class Usuário {
+    int multiplicando;
+    int multiplicadorInicial;
+    int multiplicadorFinal;
+    int temporario;
+    int resultado;
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe o valor do multiplicando: ");
+        multiplicando = leitor.nextInt();
+
+        System.out.println("Informe o valor do multiplicador inicial: ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.println("Informe o valor do multiplicador final: ");
+        multiplicadorFinal = leitor.nextInt();
+
+    }
+
 }
