4bcdc607bbe3a9be40f7b7b3cb2161dfd2f4a102
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2025-11-03 17:01:32 -0300

    Programa em andamento

--- src/br/senai/sp/jandira/tabuada/TabuadaApp.java
+++ src/br/senai/sp/jandira/tabuada/TabuadaApp.java
@@ -1,7 +1,10 @@
 package br.senai.sp.jandira.tabuada;
 
+import br.senai.sp.jandira.tabuada.model.Usuário;
+
 public class TabuadaApp {
     public static void main(String[] args) {
-
+        Usuário usuario = new Usuário();
+        usuario.obterDados();
     }
 }
--- src/br/senai/sp/jandira/tabuada/model/Usuário.java
+++ src/br/senai/sp/jandira/tabuada/model/Usuário.java
@@ -6,28 +6,51 @@ public class Usuário {
     int multiplicando;
     int multiplicadorInicial;
     int multiplicadorFinal;
-    int temporario;
+    String[] tabuada;
+    int tamanho;
+    int apoio;
     int resultado;
+    int i;
 
     public void obterDados(){
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Informe o valor do multiplicando: ");
+        System.out.print("Informe o valor do multiplicando: ");
         multiplicando = leitor.nextInt();
 
-        System.out.println("Informe o valor do multiplicador inicial: ");
+        System.out.print("Informe o valor do multiplicador inicial: ");
         multiplicadorInicial = leitor.nextInt();
 
-        System.out.println("Informe o valor do multiplicador final: ");
+        System.out.print("Informe o valor do multiplicador final: ");
         multiplicadorFinal = leitor.nextInt();
 
-        verificarMultiplicadores();
+        calcularTabuada();
     }
-    public void verificarMultiplicadores(){
-        if (multiplicadorInicial > multiplicadorFinal){
-            temporario = multiplicadorInicial;
+    public void calcularTabuada(){
+        if (multiplicadorInicial > multiplicadorFinal) {
+            apoio = multiplicadorInicial;
             multiplicadorInicial = multiplicadorFinal;
-            multiplicadorFinal = temporario;
+            multiplicadorFinal = apoio;
+        }
+        tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+        tabuada = new String[tamanho];
+
+        while (i < tamanho){
+            resultado = multiplicando * multiplicadorInicial;
+            tabuada[i] = multiplicando + " x " + multiplicadorInicial + " = " + resultado;
+            multiplicadorInicial++;
+            i = i + 1;
+
+            exibirTabuada();
+        }
+    }
+    public void exibirTabuada(){
+        System.out.println("Resultado da sua tabuada:");
+
+        i = 0;
+        while (i < tamanho){
+            System.out.println(tabuada[i]);
+            i++; //é a mesma coisa de "i = i + 1"
         }
     }
 
